532769857ef3ee862e58d498c5f9a474dd912403
Bhavesh3456 <135241733+Bhavesh3456@users.noreply.github.com>
2025-01-12 17:19:24 +0530

    django assignment

---
+++ django_site/myproject/myapp/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    product_id=models.CharField(max_length=10)
+    product_name=models.CharField(max_length=10)
